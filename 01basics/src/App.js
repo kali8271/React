d79d202aae38fd6d88df228a84a54c6affc16dc7
KALIMULLAH ANSARI <akalimullah900@gmail.com>
2025-01-13 17:00:32 +0530

    updated

--- 01basics/src/App.js
+++ 01basics/src/App.js
@@ -1,11 +1,32 @@
-
+import { useState } from 'react'
 import './App.css';
 
 function App() {
-  return (
+  
+
+    let [counter, setcounter] = useState(15)
+
+    const addValue =() =>{
+        setcounter(counter +1)
+    }
+
+    const subtractValue =() =>{
+        if(counter > 0){
+            setcounter(counter -1)
+        }
+    }
    
-        <h1>React projects</h1>
-       
+        
+
+        return (
+           <div>
+             <p>This is a simple React Counter App</p>
+             <h1>React Counter</h1>
+                <h2>Counter: {counter}</h2>
+                <button onClick={addValue}>Add</button>
+                <button onClick={subtractValue}>Subtract</button>
+           </div>
+      
      
   );
 }
